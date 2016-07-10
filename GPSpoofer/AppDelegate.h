f054e01fc487f2fec01d64453ba263f118fcae9f
Drew Conner <drew@connermail.com>
2016-07-10 19:54:39 -0400

    first commit

---
+++ GPSpoofer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPSpoofer
+//
+//  Created by Drew Conner on 7/10/16.
+//  Copyright © 2016 return7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GPSpoofer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPSpoofer
+//
+//  Created by Drew Conner on 7/10/16.
+//  Copyright © 2016 return7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
